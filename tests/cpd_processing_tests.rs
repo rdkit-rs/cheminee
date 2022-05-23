8196092f044a7fd0a072489a5ba06d9b1c66a143
Javier Pineda <javier@scientist.com>
2022-05-23 11:16:58 -0500

    added more compound processing functions

--- tests/cpd_processing_tests.rs
+++ tests/cpd_processing_tests.rs
@@ -15,3 +15,20 @@ fn test_smi_stdz() {
     let canon_taut = smi_stdz(&smiles);
     println!("{:?}", canon_taut.as_smile());
 }
+
+#[test]
+fn test_get_tautomers() {
+    let smiles = "Oc1c(cccc3)c3nc2ccncc12";
+    let romol = ROMol::from_smile(smiles).unwrap();
+    let tauts = get_tautomers(&romol);
+    for taut in tauts {
+        println!("{:?}", taut.as_smile());
+    }
+}
+
+#[test]
+fn test_process_cpd() {
+    let smiles = "Oc1c(cccc3)c3nc2ccncc12";
+    let (proc_smiles, fingerprint, descriptors) = process_cpd(smiles);
+    println!("{:?}", proc_smiles);
+}
