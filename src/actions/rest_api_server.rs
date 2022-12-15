c00579f12e6ec4ed3c83bac02abbb41cbb47ae31
Maria Dubyaga <kraftaa@gmail.com>
2022-12-15 17:56:37 -0500

    not working poem test

--- src/actions/rest_api_server.rs
+++ src/actions/rest_api_server.rs
@@ -89,3 +89,94 @@ pub async fn action(matches: &clap::ArgMatches) -> eyre::Result<()> {
 
     Ok(())
 }
+
+
+// #[handler]
+// pub async fn graphql_handler(schema: Data<&AppSchema>, req:  Json<Vec<Smile>>) -> StandardizeResponse {
+//     Json(schema.execute(req.0).await)
+// }
+
+// {
+// "smile": "CC=CO"
+// }
+// ]'
+// [{"smile":"CCC=O"}]%
+
+#[handler]
+async fn index() -> Json<Vec<Smile>> {
+    Json( vec! [Smile {
+        smile:  "CC=CO".to_string(),
+        // smile:  "XXX".to_string(),
+    }])
+}
+
+
+#[tokio::test]
+async fn health() {
+    // let app = Route::new().at("/", index);
+
+    let app = Route::new().at("/", post(index));
+    let client = TestClient::new(app);
+//
+    // let resp = client.get("/").send().await;
+//
+// // check the status code
+//     resp.assert_status_is_ok();
+// // check the json
+//     let json = resp.json().await;
+//     let json_value = json.value();
+//     println!("{:?}", json_value);
+//
+
+    use serde_json::Value;
+    let schema = "[{ \"smile\": \"CC=CO\"}]";
+    let mut object: Value = serde_json::from_str(schema).unwrap();
+    let resp = client
+        // .post("/")
+        .post("/")
+        .data(object)  // <<<----- here
+        // .body(r#"{"query": "{ health { status } }" }"#)
+        //     .body(r#"[{ "smile": "CC=CO"}]"#)
+        .send()
+        .await;
+
+
+    resp.assert_status_is_ok();
+// check the body string
+
+    resp.assert_text("CCC=O").await;
+    println!("lllla")
+}
+
+extern crate reqwest;
+use reqwest::header;
+use reqwest::Client;
+#[tokio::test]
+async fn response_test() {
+    let mut headers = header::HeaderMap::new();
+    headers.insert("accept", "application/json".parse().unwrap());
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    // let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
+    let res = client.post("http://localhost:3000/api/standardize")
+        .headers(headers)
+        // .query()
+        .body(r#"
+[
+ {
+ "smile": "CC=CO"
+ }
+]
+"#
+        )
+        .send()
+        .await
+        .unwrap();
+    // .text();
+    println!("{:?}", res);
+    println!("{:?}", res.url());
+    // Response { url: Url { scheme: "http", cannot_be_a_base: false, username: "", password: None, host: Some(Domain("localhost")), port: Some(3000), path: "/api/standardize", query: None, fragment: None }, status: 200, headers: {"content-type": "application/json; charset=utf-8", "content-length": "19", "date": "Thu, 15 Dec 2022 22:28:30 GMT"} }
+
+    // Ok(())
+}
