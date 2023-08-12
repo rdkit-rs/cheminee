e2da97867cd5a0f41505410df48c8a22c25a9c96
Xavier Lange <xrlange@gmail.com>
2023-08-12 15:42:58 -0400

    fix tests and reenable workflows

--- tests/index_management_tests.rs
+++ tests/index_management_tests.rs
@@ -2,7 +2,7 @@ use cheminee::indexing::index_manager::IndexManager;
 
 #[test]
 fn test_index_management() -> eyre::Result<()> {
-    let temp_index_dir = tempdir::TempDir::new("/tmp")?;
+    let temp_index_dir = tempdir::TempDir::new("cheminee-tests")?;
     let manager = IndexManager::new(temp_index_dir.path(), true)?;
 
     assert!(manager.exists("foo")?.is_none());
