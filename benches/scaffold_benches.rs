a08aa28de19a98e3d0d8fd62983b526953ee23f5
Javier Pineda <javier@scientist.com>
2024-09-20 08:49:31 -0500

    add benchmark for scaffold search (note: it is now 2.5x faster than before)

---
+++ benches/scaffold_benches.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+use cheminee::search::compound_processing::process_cpd;
+use cheminee::search::scaffold_search::{scaffold_search, PARSED_SCAFFOLDS};
+use test::Bencher;
+
+#[bench]
+fn bench_scaffold_search(b: &mut Bencher) {
+    let smiles =
+        "C[S+](CC[C@@H](C(=O)[O-])[NH3+])C[C@@H]1[C@H]([C@H]([C@@H](O1)N2C=NC3=C(N=CN=C32)N)O)O";
+    let (mol, fp, _descriptors) = process_cpd(smiles, false).unwrap();
+
+    b.iter(|| scaffold_search(&fp.0, &mol, &PARSED_SCAFFOLDS));
+}
