4fb6d9b1b546315b207cc4b042cfe87fcd970f73
Javier Pineda <javier@scientist.com>
2024-09-20 14:58:02 -0500

    more multithreading amidst structure searching

--- src/search/identity_search.rs
+++ src/search/identity_search.rs
@@ -1,12 +1,13 @@
-use std::collections::{HashMap, HashSet};
-
-use bitvec::prelude::{BitSlice, Lsb0};
-use rdkit::ROMol;
-use regex::Regex;
-use tantivy::Searcher;
-
 use crate::search::structure_matching::exact_match;
 use crate::search::{basic_search::basic_search, STRUCTURE_MATCH_DESCRIPTORS};
+use bitvec::prelude::{BitSlice, Lsb0};
+use rayon::prelude::*;
+use rdkit::ROMol;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, Mutex};
+use tantivy::schema::Field;
+use tantivy::{DocAddress, Searcher};
 
 pub fn identity_search(
     searcher: &Searcher,
@@ -28,45 +29,76 @@ pub fn identity_search(
     let fingerprint_field = schema.get_field("fingerprint")?;
     let extra_data_field = schema.get_field("extra_data")?;
 
-    let mut filtered_results: HashSet<(String, String)> = HashSet::new();
+    let query_mol_mutex = Arc::new(Mutex::new(query_mol.clone()));
 
-    for docaddr in initial_results {
-        let doc = searcher.doc(docaddr)?;
+    let filtered_results = initial_results
+        .into_par_iter()
+        .filter_map(|result| {
+            let confirmed_match = identity_match(
+                result,
+                smiles_field,
+                fingerprint_field,
+                extra_data_field,
+                &searcher,
+                &query_mol_mutex.lock().unwrap(),
+                query_fingerprint,
+                use_chirality,
+            );
 
-        let smiles = doc
-            .get_first(smiles_field)
-            .ok_or(eyre::eyre!("Tantivy smiles retrieval failed"))?
-            .as_text()
-            .ok_or(eyre::eyre!("Failed to stringify smiles"))?;
-
-        let fingerprint = doc
-            .get_first(fingerprint_field)
-            .ok_or(eyre::eyre!("Tantivy fingerprint retrieval failed"))?
-            .as_bytes()
-            .ok_or(eyre::eyre!("Failed to read fingerprint as bytes"))?;
-
-        let fingerprint_bits = BitSlice::<u8, Lsb0>::from_slice(fingerprint);
-        let fp_match = query_fingerprint == fingerprint_bits;
-
-        if fp_match {
-            let mol_exact_match =
-                exact_match(&ROMol::from_smiles(smiles)?, query_mol, use_chirality);
-            if mol_exact_match {
-                let extra_data = match doc.get_first(extra_data_field) {
-                    Some(extra_data) => serde_json::to_string(
-                        extra_data
-                            .as_json()
-                            .ok_or(eyre::eyre!("Failed to jsonify extra data"))?,
-                    )?,
-                    None => "".to_string(),
-                };
-
-                filtered_results.insert((smiles.to_string(), extra_data));
+            if let Ok(confirmed_match) = confirmed_match {
+                confirmed_match
+            } else {
+                None
             }
+        })
+        .collect::<HashSet<(String, String)>>();
+
+    Ok(filtered_results)
+}
+
+pub fn identity_match(
+    docaddr: DocAddress,
+    smiles_field: Field,
+    fingerprint_field: Field,
+    extra_data_field: Field,
+    searcher: &Searcher,
+    query_mol: &ROMol,
+    query_fingerprint: &BitSlice<u8>,
+    use_chirality: bool,
+) -> eyre::Result<Option<(String, String)>> {
+    let doc = searcher.doc(docaddr)?;
+
+    let smiles = doc
+        .get_first(smiles_field)
+        .ok_or(eyre::eyre!("Tantivy smiles retrieval failed"))?
+        .as_text()
+        .ok_or(eyre::eyre!("Failed to stringify smiles"))?;
+
+    let fingerprint = doc
+        .get_first(fingerprint_field)
+        .ok_or(eyre::eyre!("Tantivy fingerprint retrieval failed"))?
+        .as_bytes()
+        .ok_or(eyre::eyre!("Failed to read fingerprint as bytes"))?;
+
+    let fingerprint_bits = BitSlice::<u8, Lsb0>::from_slice(fingerprint);
+    let fp_match = query_fingerprint == fingerprint_bits;
+
+    if fp_match {
+        let mol_exact_match = exact_match(&ROMol::from_smiles(smiles)?, query_mol, use_chirality);
+        if mol_exact_match {
+            let extra_data = match doc.get_first(extra_data_field) {
+                Some(extra_data) => serde_json::to_string(
+                    extra_data
+                        .as_json()
+                        .ok_or(eyre::eyre!("Failed to jsonify extra data"))?,
+                )?,
+                None => "".to_string(),
+            };
+            return Ok(Some((smiles.to_string(), extra_data)));
         }
     }
 
-    Ok(filtered_results)
+    Ok(None)
 }
 
 pub fn build_identity_query(
