0e97efe8df9e5f0ca01a9c61fe1a28410c7295fb
Xavier Lange <xrlange@gmail.com>
2024-09-26 19:52:24 -0400

    does it blend

--- tests/index_management_tests.rs
+++ tests/index_management_tests.rs
@@ -1,11 +1,24 @@
 use cheminee::indexing::index_manager::IndexManager;
 
 #[test]
-fn test_index_management() -> eyre::Result<()> {
+fn test_missing_index() -> eyre::Result<()> {
     let temp_index_dir = tempdir::TempDir::new("cheminee-tests")?;
     let manager = IndexManager::new(temp_index_dir.path(), true)?;
 
     assert!(manager.exists("foo")?.is_none());
+
+    Ok(())
+}
+
+#[test]
+fn test_creating_index() -> eyre::Result<()> {
+    let temp_index_dir = tempdir::TempDir::new("cheminee-tests")?;
+    let manager = IndexManager::new(temp_index_dir.path(), true)?;
+
+    let schema = cheminee::schema::LIBRARY.get("descriptor_v1").unwrap();
+    let index = manager.create("test-index", schema, false, None)?;
+
+    assert_eq!(&index.schema(), schema);
 
     Ok(())
 }
