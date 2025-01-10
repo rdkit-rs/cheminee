03ba14eb9cbbf2738337c6c84e5d9f68aa2df973
Javier Pineda <javier@scientist.com>
2025-01-10 15:52:18 -0600

    previously, index-sdf was resetting the index each time --> converted this to an optional functionality

--- src/command_line/indexing/index_sdf.rs
+++ src/command_line/indexing/index_sdf.rs
@@ -2,6 +2,7 @@ use crate::command_line::prelude::*;
 use rayon::prelude::*;
 use rdkit::{MolBlockIter, RWMol};
 use std::sync::{Arc, Mutex};
+use tantivy::directory::MmapDirectory;
 
 pub const NAME: &str = "index-sdf";
 
@@ -36,6 +37,12 @@ pub fn command() -> Command {
                 .num_args(1),
         )
         .arg(
+            Arg::new("create-or-reset-index")
+                .required(false)
+                .long("create-or-reset-index")
+                .num_args(0),
+        )
+        .arg(
             Arg::new("commit")
                 .required(false)
                 .long("commit")
@@ -52,6 +59,7 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
         .ok_or(eyre::eyre!("Failed to extract index path"))?;
     let limit = matches.get_one::<String>("limit");
     let chunksize = matches.get_one::<String>("chunk-size");
+    let reset_index: bool = matches.get_flag("create-or-reset-index");
     let commit: bool = matches.get_flag("commit");
 
     let chunksize = if let Some(chunksize) = chunksize {
@@ -67,14 +75,6 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
         limit
     );
 
-    let index_dir_metadata = std::fs::metadata(index_dir);
-    if let Ok(metadata) = index_dir_metadata {
-        if metadata.is_dir() {
-            std::fs::remove_dir_all(index_dir)?;
-        }
-    }
-    std::fs::create_dir(index_dir)?;
-
     let mol_iter = MolBlockIter::from_gz_file(sdf_path, true, true, false)
         .map_err(|e| eyre::eyre!("could not read gz file: {:?}", e))?;
 
@@ -87,7 +87,14 @@ pub fn action(matches: &ArgMatches) -> eyre::Result<()> {
     let schema = crate::schema::LIBRARY
         .get("descriptor_v1")
         .ok_or(eyre::eyre!("Failed to extract schema"))?;
-    let index = create_or_reset_index(index_dir, schema)?;
+
+    let index = if reset_index {
+        create_or_reset_index(index_dir, schema)?
+    } else {
+        let mmap_directory = MmapDirectory::open(index_dir)?;
+        tantivy::Index::open(mmap_directory)?
+    };
+
     let mut index_writer = index.writer_with_num_threads(1, 50 * 1024 * 1024)?;
 
     let mut counter = 0;
